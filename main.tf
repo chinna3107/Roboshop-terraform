fd2dd5093713cf31b9708e3c95137c7c27f80774
chinnapareddy <chinna.aduri@gmail.com>
2023-10-15 05:05:09 -0700

    state roboshop

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "component" {
    source   = "git::https://github.com/chinna3107/tf-module-vpc.git"
    for_each = var.component
 
-   zone_id        = var.zone_id
-   security_group = var.Security_group
+   zone_id         = var.zone_id
+   security_group  = var.Security_group
    name           = each.value["name"]
    instance_type  = each.value["instance_type"]
 
